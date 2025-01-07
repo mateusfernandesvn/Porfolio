6c0dcf418104a4484defa5d2f5774f538e722566
Mateus Fernandes <mateusveloso2608@gmail.com>
2025-01-06 22:26:18 -0300

    fixing animation

--- src/app/components/section/index.tsx
+++ src/app/components/section/index.tsx
@@ -31,7 +31,7 @@ export function Section({ id, children }: SectionProps) {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }} 
+      initial={{ opacity: 0, y: 30 }} 
       animate={{
         opacity: isInView ? 1 : 0, 
         y: isInView ? 0 : 50, 
@@ -41,7 +41,7 @@ export function Section({ id, children }: SectionProps) {
         ease: "easeOut", 
       }}
       id={id}
-      className="w-full flex flex-col items-center justify-center my-6 h-auto p-12 max-md:px-4 max-md:mt-0"
+      className="w-full flex flex-col items-center justify-center my-6 h-auto p-11 max-md:px-4 max-md:mt-0"
     >
       {children}
     </motion.div>
